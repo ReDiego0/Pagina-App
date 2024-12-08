f4b96c758f2f1f0e8fbb61c6875f72009e95dae2
ReDiego0 <134623797+ReDiego0@users.noreply.github.com>
2024-12-08 17:59:02 -0300

    Agregada pagina de preguntas frecuentes y arreglado el footer

--- website_app/home/urls.py
+++ website_app/home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('faq/', views.faq, name='faq'),
     path('home/', views.home, name='home'),
 ]
--- website_app/home/views.py
+++ website_app/home/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def home(request):
     template = loader.get_template('inicio.html')
     return HttpResponse(template.render())
+
+def faq(request):
+    template = loader.get_template('faq.html')
+    return HttpResponse(template.render())
